980b4cc1057d4b2534ae1924e1c828c44a3c7d40
spidedev <dorian.caball2002@gmail.com>
2023-09-01 20:54:15 -0600

    Movement, Dialogue Manager
    also sounds, and easy npcs :D

---
+++ Assets/Objects/ObjectSettings.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Objects/Object Settings")]
+
+public class ObjectSettings : ScriptableObject
+{
+    public enum Type
+    {
+        NPC,
+        Object
+    }
+
+    public Type type;
+    
+    [Header("Prompt Settings")]
+    public bool CanCooldown;
+    public float MaxTimeToPressDown, WaitAfterCooldown;
+}
